a7b4cc3e1a1b3ad34d82aa3c7c66c6c8f093a79e
Bruno Basto <bruno.basto@liferay.com>
2017-07-03 19:00:39 -0300

    Compiles Java faster

--- tasks/java.js
+++ tasks/java.js
@@ -5,26 +5,45 @@ const configs = require('./lib/configs');
 const duration = require('gulp-duration');
 const gulp = require('gulp');
 const gutil = require('gulp-util');
+const projectDeps = require('./lib/projectDeps');
+
+const buildGradleArgs = (projects) => {
+	const skippedTasks = [
+		'transpileJS', 'configJSModules', 'npmInstall', 'downloadMetalCli', 'buildCSS', 'downloadNode', 'jar'
+	];
+	const args = ['compileJava'];
+	projects.forEach((project) => skippedTasks.forEach((task) => {
+		args.push('-x');
+		args.push(project + ':' + task);
+	}));
+	skippedTasks.forEach((task) => {
+		args.push('-x');
+		args.push(task);
+	});
+	return args;
+};
 
 gulp.task('build-java', (done) => {
 	const javaTimer = duration('java');
-	gutil.log(gutil.colors.magenta('java'), 'Compiling Java');
-	const cp = childProcess.spawn('gradle', ['compileJava'], { cwd: process.cwd() });
-	let stdErr = '';
-	cp.stderr.on('data', (chunk) => {
-		stdErr += chunk.toString();
-	});
-	cp.on('exit', (code) => {
-		if (code === 0) {
-			gulp.src(configs.globClass)
-			.pipe(javaTimer)
-			.pipe(gulp.dest(configs.pathExploded))
-			.on('end', () => done());
-		}
-		else {
-			console.log(stdErr);
-			gutil.log(gutil.colors.magenta('java'), gutil.colors.red('Errors compiling Java. Check compiler output.'));
-			done();
-		}
+	projectDeps().then((projects) => {
+		gutil.log(gutil.colors.magenta('java'), 'Compiling Java');
+		const cp = childProcess.spawn('gradle', buildGradleArgs(projects), { cwd: process.cwd() });
+		let stdErr = '';
+		cp.stderr.on('data', (chunk) => {
+			stdErr += chunk.toString();
+		});
+		cp.on('exit', (code) => {
+			if (code === 0) {
+				gulp.src(configs.globClass)
+				.pipe(javaTimer)
+				.pipe(gulp.dest(configs.pathExploded))
+				.on('end', () => done());
+			}
+			else {
+				console.log(stdErr);
+				gutil.log(gutil.colors.magenta('java'), gutil.colors.red('Errors compiling Java. Check compiler output.'));
+				done();
+			}
+		});
 	});
 });
---
+++ tasks/lib/projectDeps.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const childProcess = require('child_process');
+
+module.exports = () => {
+	return new Promise((resolve, reject) => {
+		if (global.projectDeps) {
+			resolve(global.projectDeps);
+		}
+		else {
+			const cp = childProcess.spawn('gradle', ['dependencies', '--configuration', 'compile'], { cwd: process.cwd() });
+			let gradleOutput = '';
+			cp.stdout.on('data', (data) => {
+				gradleOutput += data.toString();
+			});
+			cp.on('exit', (code) => {
+				if (code === 0) {
+					let projectDeps = gradleOutput.split('\n')
+					.filter(line => line.indexOf('project :') > -1)
+					.map(depLine => depLine.replace(/(\+|\\)\-\-\- project /, ''));
+					global.projectDeps = projectDeps;
+					resolve(projectDeps);
+				}
+				else {
+					reject(new Error('Unable to call gardle to get compile dependencies.'));
+				}
+			});
+		}
+	});
+};
