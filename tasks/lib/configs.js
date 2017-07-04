b9bc6a44911aad0521ef7739469267054a240efc
Bruno Basto <bruno.basto@liferay.com>
2017-07-04 10:58:32 -0300

    Adds support for .lwatch.json on the user home folder. User configuration can be overridden by project specific configurations

--- tasks/lib/configs.js
+++ tasks/lib/configs.js
@@ -4,11 +4,15 @@ const fs = require('fs');
 const gutil = require('gulp-util');
 const path = require('path');
 
-const userConfigsPath = path.join(process.cwd(), '.lwatch.json');
-let userConfigs = {};
+let configs = require('../../config.json');
+const userConfigsPath = path.resolve(require('os').homedir(), '.lwatch.json');
+console.log(userConfigsPath);
 if (fs.existsSync(userConfigsPath)) {
-	userConfigs = require(userConfigsPath);
+	configs = Object.assign(configs, require(userConfigsPath));
 }
-const configs = Object.assign(require('../../config.json'), userConfigs);
+const projectConfigsPath = path.join(process.cwd(), '.lwatch.json');
+if (fs.existsSync(projectConfigsPath)) {
+	configs = Object.assign(configs, require(projectConfigsPath));
+}
 gutil.log(gutil.colors.magenta('configs'), configs);
 module.exports = configs;
