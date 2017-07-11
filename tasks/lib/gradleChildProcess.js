5dc3255f0203ec932a09e6303c2beb21ec01b494
drewbrokke <drew.brokke@liferay.com>
2017-07-10 17:23:59 -0700

    issue 9 - adds gradleChildProcess.js to automatically rename a settings.gradle file if it found in the parent directory

---
+++ tasks/lib/gradleChildProcess.js
@@ -0,0 +1,24 @@
+const childProcess = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (args) => {
+	const cwd = process.cwd();
+
+	const gradleSettingsFilePath = path.join(cwd, '..', 'settings.gradle');
+
+	if (fs.existsSync(gradleSettingsFilePath)) {
+		const gradleSettingsTempFilePath = gradleSettingsFilePath + '.tmp';
+
+		fs.rename(gradleSettingsFilePath, gradleSettingsTempFilePath);
+
+		const cp = childProcess.spawn('gradle', args, { cwd });
+
+		cp.on('exit', () => fs.rename(gradleSettingsTempFilePath, gradleSettingsFilePath));
+
+		return cp;
+	}
+	else {
+		return childProcess.spawn('gradle', args, { cwd });
+	}
+}
